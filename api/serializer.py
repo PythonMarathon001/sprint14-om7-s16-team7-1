fc596e88fc72a31381061982b02b2ff2f5927b66
MaximLymych <limichmaxim@gmail.com>
2022-04-09 13:52:27 +0300

    Done CRUD user

--- api/serializer.py
+++ api/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from author.models import Author
 from book.models import Book
 from order.models import Order
+from authentication.models import CustomUser
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -20,3 +21,10 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ('id', 'user', 'book', 'created_at', 'end_at', 'plated_end_at')
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'first_name', 'middle_name', 'last_name', 'email', 'password',
+                  'updated_at', 'created_at', 'role', 'is_active')
